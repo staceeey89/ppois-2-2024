de6ee7b9d533d8fd326c44b3b628c744017e82d7
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2024-03-14 20:56:41 +0300

    Update controller.py

--- lab2/controller.py
+++ lab2/controller.py
@@ -1,6 +1,6 @@
 from tkinter import Button
 from view import create_root
-from model import show_train_data, write_in_file, Search_Info, delete_data, show_train_data_table, show_train_data_tree
+from model import show_train_data, write_in_file, Search_Info, delete_data, show_train_data_table, show_train_data_tree, create_database, load_from_db, insert_to_db, show_train_data_from_db
 
 def main():
     root = create_root()
@@ -22,6 +22,18 @@ def main():
 
     btn_delete_data = Button(root, text='Удалить данные', command=lambda: delete_data(root))
     btn_delete_data.pack()
+
+    btn_create_db = Button(root, text='Создать БД', command=create_database)
+    btn_create_db.pack()
+
+    btn_load_from_db = Button(root, text='Загрузить из БД', command=load_from_db)
+    btn_load_from_db.pack()
+
+    btn_insert_to_db = Button(root, text='Сохранить в БД', command=insert_to_db)
+    btn_insert_to_db.pack()
+
+    btn_print_db = Button(root, text='Вывести информацию из БД', command=show_train_data_from_db)
+    btn_print_db.pack()
 
     root.mainloop()
 
